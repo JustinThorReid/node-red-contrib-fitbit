a6d0cde06d0262ae6f09707887595532a135552f
Justin Reid <phatness0@gmail.com>
2020-01-03 16:11:53 -0500

    Create initial node

---
+++ fitbit-query.js
@@ -0,0 +1,10 @@
+module.exports = function (RED) {
+    function fitbitQueryNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        node.on('input', function (msg) {
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("lower-case", fitbitQueryNode);
+}
