1dc9e97e76c3028a16ea1789c1ff73be03609439
Maxim Katargin <maxim.katargin@kuehne-nagel.com>
2021-04-11 14:25:37 +0300

    Added activity logging and removing

---
+++ test/UrlFactory/deleteActivity_spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const expect = require('chai').expect;
+const UrlFactory = require("../../UrlFactory");
+
+describe('UrlFactory.deleteActivity', () => {
+
+  // XXX kmv to be done
+
+});
---
+++ test/UrlFactory/logActivity_spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const expect = require('chai').expect;
+const UrlFactory = require("../../UrlFactory");
+
+describe('UrlFactory.logActivity', () => {
+
+  // XXX kmv to be done
+
+});
